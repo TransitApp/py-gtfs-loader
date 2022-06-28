6c52e0691f062e3bfbc3606215c5e4fdb74f6ce3
Jonathan Milot <jonathan@transit.app>
2022-06-28 15:06:08 -0400

    add simple gtfs loader

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='py-gtfs-loader',
+      version='0.0.1',
+      description='Load GTFS',
+      url='https://github.com/TransitApp/py-gtfs-loader',
+      author='Nicholas Paun, Jonathan Milot',
+      license='License :: OSI Approved :: MIT License',
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=[])

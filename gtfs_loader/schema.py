4a2784fd6b9437a5c47e9cd613b95afa84defb2b
Jonathan Milot <jonathan@transit.app>
2022-11-08 10:25:36 -0500

    set default value to None ?

--- gtfs_loader/schema.py
+++ gtfs_loader/schema.py
@@ -231,10 +231,10 @@ class StopTime(Entity):
     end_pickup_drop_off_window: GTFSTime = GTFSTime('')
     pickup_type: PickupType = PickupType.REGULARLY_SCHEDULED
     drop_off_type: DropOffType = DropOffType.REGULARLY_SCHEDULED
-    mean_duration_factor: float
-    mean_duration_offset: float
-    safe_duration_factor: float
-    safe_duration_offset: float
+    mean_duration_factor: float = None
+    mean_duration_offset: float = None
+    safe_duration_factor: float = None
+    safe_duration_offset: float = None
 
     @property
     def stop(self):

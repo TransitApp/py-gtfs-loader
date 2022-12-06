90d71bbcb6f4d982fbee96d080d960e87de65da0
Jonathan Milot <jonathan@transit.app>
2022-12-06 15:49:20 -0500

    Overwrite file with no entities with an  empty csv

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='py-gtfs-loader',
-      version='0.1.8',
+      version='0.1.9',
       description='Load GTFS',
       url='https://github.com/TransitApp/py-gtfs-loader',
       author='Nicholas Paun, Jonathan Milot',
